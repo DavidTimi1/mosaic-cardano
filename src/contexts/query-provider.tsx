1b23e0df5627c7fa6af3fced047d9f529cd9cdd2
Dev_id <duwagbale07@gmail.com>
2026-05-20 20:42:48 +0100

    feat: create global query provider client for app

---
+++ src/contexts/query-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactNode } from 'react'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+});
+
+export default function QueryProvider({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
