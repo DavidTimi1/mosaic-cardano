e3bc2acae9192223b3269b6906146335d19b6f7c
Dev_id <duwagbale07@gmail.com>
2026-05-27 16:20:31 +0100

    refactor: change hardcoded links in app to use enums for routes

---
+++ src/lib/routes.ts
@@ -0,0 +1,13 @@
+export const ROUTES = {
+  HOME: '/home',
+  EXPLORE: '/explore',
+  AUTH: '/auth',
+  NEW_COMMUNITY: '/new',
+  SETTINGS: '/settings',
+  SUPPORT: '/support',
+  STUDIO: '/studio',
+  PROFILE: '/profile', // generic profile
+  NOTIFICATIONS: '/notifications',
+  VILLAGE: (id: string) => `/v/${id}`,
+  USER: (id: string, currentUserId?: string) => id === currentUserId ? '/profile' : `/u/${id}`,
+} as const;
