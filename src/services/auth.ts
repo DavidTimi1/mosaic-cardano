38dba8d639e17c4f65ead0ef21fe2cfe0f4b4ec6
Dev_id <duwagbale07@gmail.com>
2026-05-30 13:35:21 +0000

    feat(auth): refactor AuthView to use hooks for login and registration, enhance form handling

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,19 +1,54 @@
+import { API } from "@/lib/api-routes";
 import { useXQuery } from "@/lib/extended-react-query";
 import { AuthStateResponseSchema, type AuthStateResponse } from "@/types/api";
+import { fetchAPI } from "./api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useGetAuthState = () => {
   return useXQuery({
     queryKey: ['authState'],
-    queryFn: async () => {
-      const res = await fetch('/api/auth/me', {
-        credentials: 'include',
-      });
+    queryFn: getAuthState
+  });
+};
 
-      const payload = await res.json();
-      return AuthStateResponseSchema.parse(payload) satisfies AuthStateResponse;
+export const useRegister = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['authState']
+      });
     }
   });
 };
+
+export const useLogin = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['authState']
+      });
+    }
+  });
+}
+
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['authState']
+      });
+    }
+  });
+}
 
 export const useGetVillageMembership = (communityId: string) => {
   return useXQuery({
@@ -30,3 +65,41 @@ export const useGetVillageMembership = (communityId: string) => {
     }
   });
 };
+
+
+const getAuthState = async () => {
+  const res = await fetchAPI(API.AUTH.WHOAMI);
+  return AuthStateResponseSchema.parse(res) satisfies AuthStateResponse;
+};
+
+const login = async ({email, password}: { email: string; password: string }) => {
+  const res = await fetchAPI(API.AUTH.LOGIN, {
+    method: 'POST',
+    data: { email, password },
+  });
+
+  return AuthStateResponseSchema.parse(res) satisfies AuthStateResponse;
+}
+
+
+interface registerPayload {
+  email: string;
+  username: string;
+  displayName: string;
+  password: string;
+}
+
+const register = async (payload: registerPayload) => {
+  const res = await fetchAPI(API.AUTH.REGISTER, {
+    method: 'POST',
+    data: payload,
+  });
+
+  return AuthStateResponseSchema.parse(res) satisfies AuthStateResponse;
+}
+
+const logout = async () => {
+  await fetchAPI(API.AUTH.LOGOUT, {
+    method: 'POST',
+  });
+}
